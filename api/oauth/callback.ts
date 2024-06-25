5fb47ba35bb3ab85b05419f10d8da2ca31842ace
Wijnand Karsens <wijnand@karsens.com>
2024-06-25 21:38:49 +0200

    Improvements

--- api/oauth/callback.ts
+++ api/oauth/callback.ts
@@ -35,16 +35,22 @@ export const GET = async (request: Request) => {
 
   try {
     // comes from OpenAPI
-    const tokenUrl = "https://github.com/login/oauth/access_token";
+    const redirect_uri = "https://deploy.actionschema.com/oauth/callback";
+    const tokenUrl = `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${redirect_uri}`;
+
+    console.log({ tokenUrl });
     const result = await fetch(tokenUrl, {
       method: "POST",
+
+      //not sure if body or params
       body: JSON.stringify({
-        // the secret needs to be in the back...
         client_id,
+        // the secret needs to be in the back...
         client_secret,
         code,
-        redirect_uri: "https://deploy.actionschema.com/oauth/callback",
+        redirect_uri,
       }),
+
       headers: {
         Accept: "application/json",
       },
